20a67906188c7493fe3fed0e04dcc9c15f956f9c
franzidu <stfrdudi@stud.h-da.de>
2023-01-01 15:20:16 +0100

    Setup pt.1

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import './App.modules.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "../MainPage/MainPage.modules.css";
+
+function MainPage() {
+    return ( <div className={styles.MainPage}>
+        <h1>Hallo</h1>
+    </div> );
+}
+
+export default MainPage;
