a11dd92b5da8d4600267a26ab2e9de12ddb1a99e
franzidu <stfrdudi@stud.h-da.de>
2023-01-01 18:09:45 +0100

    added bootstrap, added navbar

--- portfolio/src/components/NavBar/NavBar.jsx
+++ portfolio/src/components/NavBar/NavBar.jsx
@@ -4,7 +4,27 @@ import styles from "../NavBar/NavBar.module.css";
 function NavBar() {
     return ( 
     <div className={styles.NavBar}>
-        <p>hello</p>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a className="navbar-brand" href="...">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="...">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="...">Privacy</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="...">Imprints</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div> );
 }
 
